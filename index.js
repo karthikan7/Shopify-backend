4649541c4e691a5f09652d559df3d6b631b1634a
Karthik A N <ankarthik657@gmail.com>
2026-06-26 13:33:05 +0530

    fix backend deployment issue

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-    origin: ['https://shopify-one.vercel.app', 'http://localhost:3000'],
+    origin: ['https://shopif-one.vercel.app/', 'http://localhost:3000'],
   credentials:        true,
   methods:            ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders:     ['Content-Type', 'Authorization'],
