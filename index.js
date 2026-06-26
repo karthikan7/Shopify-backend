8eb0fe81901383fadc21c72b1ca05f8dd5966668
Karthik A N <ankarthik657@gmail.com>
2026-06-26 12:21:28 +0530

    Update CORS with production Vercel URL

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-  origin:             process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: ['https://shopify-one.vercel.app', 'http://localhost:3000'],
   credentials:        true,
   methods:            ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders:     ['Content-Type', 'Authorization'],
